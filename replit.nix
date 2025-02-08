8fbea0454c009d4523247cb0e112577d8dbbc250
Sara Abid <bscs24112@itu.edu.pk>
2025-02-08 19:45:15 +0000

    Added enemies code

---
+++ replit.nix
@@ -0,0 +1,8 @@
+{pkgs}: {
+  deps = [
+    pkgs.rPackages.IlluminaHumanMethylationEPICanno_ilm10b3_hg19
+    pkgs.rPackages.IlluminaHumanMethylationEPICanno_ilm10b4_hg19
+    pkgs.raylib-games
+    pkgs.raylib
+  ];
+}
