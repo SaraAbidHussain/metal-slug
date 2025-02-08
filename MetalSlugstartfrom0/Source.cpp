1a3aded715ef01d1af2daade45e4a8c739268847
Ali Maqsood <alimaqsood35892@gmail.com>
2025-02-08 09:12:36 +0500

    feat // added loading screen animations, music and platforms

--- MetalSlugstartfrom0/Source.cpp
+++ MetalSlugstartfrom0/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include <unistd.h>
 using namespace std;
 
 // Function pointer for movement
@@ -15,8 +16,11 @@ void UpdateAndDrawPlayer();
 void FiredBullet(bool movingUp);
 void DrawBullets();
 void LoadBulletTexture();
+void InitPlatforms();
+void ShowLoadingScreen(Music& music);
 
 const int maxBullets = 10;
+
 struct bullet
 {
     float x = 0, y = 0, rad = 5.0f;
@@ -46,10 +50,10 @@ const float gravity = 800.0f;
 const float jumpStrength = -500.0f;
 
 // player dimensions
-const float playerWidth = 50.0f;  
-const float playerHeight = 50.0f; 
+const float playerWidth = 70.0f;  
+const float playerHeight = 70.0f; 
 const float startX = 300.0f;      // X position
-const float groundY = 490.0f;  // Y position
+const float groundY = 470.0f;  // Y position
 const float startY = groundY;  // for jumping to land on ground
 
 // Player initialization
@@ -75,20 +79,39 @@ int frameCounter = 0;
 const int frameSpeed = 2; // Adjust speed of animation
 
 
+// standing platforms
+struct Platform {
+    Rectangle rect;
+    float originalX;  
+    bool isActive;
+};
+
+const int maxPlatforms = 4;
+Platform platforms[maxPlatforms];
+float bgWidth;
+
+
 
 int main() {
     InitWindow(screenWidth, screenHeight, "Metal Slug!");
+    InitAudioDevice();
     LoadBulletTexture();
     SetTargetFPS(90);
+    InitPlatforms();
 
-    
     bg = LoadTexture("perfectBG1.png");
     P.leftTexture = LoadTexture("left face.png");
     P.rightTexture = LoadTexture("main_char.png");
+    Music bgAudio = LoadMusicStream("bgAudio1.mp3"); 
+    Music bgAudio1 = LoadMusicStream("bgAudio.ogg"); 
+    Sound bullets = LoadSound("bullets.mp3");
+    PlayMusicStream(bgAudio);
+
+    ShowLoadingScreen(bgAudio1);
 
     while (!WindowShouldClose()) {
 
-        
+        UpdateMusicStream(bgAudio);
         float dt = GetFrameTime();
 
         if (IsKeyDown(KEY_LEFT) && P.player.x > 0) 
@@ -114,10 +137,12 @@ int main() {
 
         if (IsKeyPressed(KEY_F))
         {
+            PlaySound(bullets);
             FiredBullet(false); 
         }
         if (IsKeyPressed(KEY_R))
         {
+            PlaySound(bullets);
             FiredBullet(true);
         }
         for (int i = 0; i < maxBullets; i++) {
@@ -135,13 +160,17 @@ int main() {
             scrollX = 0; // Looping background 
         }
 
+        for (int i = 0; i < maxPlatforms; i++) {
+            platforms[i].rect.x = platforms[i].originalX + scrollX;
+        }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         DrawScrollingBackground(bg, scrollX);
-        DrawBullets();
+        DrawBullets(); 
         UpdateAndDrawPlayer();
+              
         EndDrawing();
     }
 
@@ -176,32 +205,51 @@ void Jump() {
     if (!P.isJumping) {
         P.isJumping = true;
         P.verticalVelocity = jumpStrength;
+        P.player.y += P.verticalVelocity * GetFrameTime();  
     }
 }
 
 void applyGravity(float dt) {
-    if (P.isJumping) {
-        P.verticalVelocity += gravity * dt;
-        P.player.y += P.verticalVelocity * dt;
+    bool onPlatform = false;
+
+    for (int i = 0; i < maxPlatforms; i++) {
+        // for platforms
+        if (platforms[i].isActive &&
+            P.player.y + P.player.height >= platforms[i].rect.y &&
+            P.player.y + P.player.height <= platforms[i].rect.y + 10 &&
+            P.player.x + P.player.width > platforms[i].rect.x &&
+            P.player.x < platforms[i].rect.x + platforms[i].rect.width) {
+            
+            P.player.y = platforms[i].rect.y - P.player.height;
+            
+            if (P.verticalVelocity > 0) {
+                P.isJumping = false;
+            }
+
+            P.verticalVelocity = 0;
+            onPlatform = true;
+            break;
+        }
     }
 
-    if (P.player.y >= groundY) {
-        P.player.y = groundY;
-        P.isJumping = false;
-        P.verticalVelocity = 0;
+    if (!onPlatform) {
+        P.verticalVelocity += gravity * dt;
+        P.player.y += P.verticalVelocity * dt;
+
+        if (P.player.y >= groundY) {
+            P.player.y = groundY;
+            P.isJumping = false;
+            P.verticalVelocity = 0;
+        }
     }
 }
 
 
 void DrawScrollingBackground(Texture2D bg, float scrollX) {
-
     float bgWidth = bg.width;
-    
-   
     DrawTextureEx(bg, {scrollX, 0}, 0.0f, 1.0f, WHITE);
     DrawTextureEx(bg, {scrollX + bgWidth, 0}, 0.0f, 1.0f, WHITE);
 }
-
 
 void DrawBullets() {
     frameCounter++;
@@ -223,7 +271,6 @@ void DrawBullets() {
 }
 
 void UpdateAndDrawPlayer() {
-    
     P.hitbox.x = P.player.x;
     P.hitbox.y = P.player.y;
     P.hitbox.width = P.player.width;
@@ -237,7 +284,6 @@ void UpdateAndDrawPlayer() {
         {0, 0}, 0, WHITE
     );
 }
-
 
 void MoveBullets(struct bullet& b)
 {
@@ -276,8 +322,71 @@ void FiredBullet(bool movingUp)
     }
 }
 
-
 void LoadBulletTexture() {
     bulletTexture = LoadTexture("bullets.png");
     bulletFrameWidth = bulletTexture.width / bulletFrames;
+}
+
+// platform function
+void InitPlatforms() {
+    bgWidth = bg.width; 
+
+    platforms[0] = {{50, 493, 165, 10}, 440, true};
+    platforms[1] = {{750, 372, 175, 10}, 175, true};
+    platforms[2] = {{0, 435, 110, 10}, 0, true};
+    platforms[3] = {{1200, 493, 110, 10}, 1179, true};
+}
+
+void WaitTime(float seconds) {
+    int ms = seconds * 1000;  
+    usleep(ms * 1000);        
+}
+
+void ShowLoadingScreen(Music& music) {
+    SetTargetFPS(60);
+    PlayMusicStream(music);
+
+    Texture2D load25 = LoadTexture("load25.png");
+    Texture2D load50 = LoadTexture("load50.png");
+    Texture2D load75 = LoadTexture("load75.png");
+    Texture2D load100 = LoadTexture("load100.png");
+    Texture2D enter1 = LoadTexture("enter1.png");
+    Texture2D enter2 = LoadTexture("enter2.png");
+
+    int centerX = screenWidth / 2;
+    int centerY = screenHeight / 2;
+
+    Texture2D loadingFrames[] = {load25, load50, load75, load100};
+    for (int i = 0; i < 4; i++) {
+        float startTime = GetTime();
+        // loop will run for 0.5 seconds for each frame
+        while (GetTime() - startTime < 0.5f) {
+            UpdateMusicStream(music);
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawTexture(loadingFrames[i], centerX - loadingFrames[i].width / 2, centerY - loadingFrames[i].height / 2, WHITE);
+            EndDrawing();
+        }
+    }
+
+    while (!IsKeyPressed(KEY_ENTER) && !WindowShouldClose()) {
+        UpdateMusicStream(music);
+        BeginDrawing();
+        ClearBackground(BLACK);
+        // even and odd for enter buttons
+        if ((int)(GetTime() * 2) % 2 == 0) {
+            DrawTexture(enter1, centerX - enter1.width / 2, centerY - enter1.height / 2, WHITE);
+        }
+         else {
+            DrawTexture(enter2, centerX - enter2.width / 2, centerY - enter2.height / 2, WHITE);
+        }
+        EndDrawing();
+    }
+    
+    UnloadTexture(load25);
+    UnloadTexture(load50);
+    UnloadTexture(load75);
+    UnloadTexture(load100);
+    UnloadTexture(enter1);
+    UnloadTexture(enter2);
 }
